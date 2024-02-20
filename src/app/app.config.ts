a686f9b2cd1f79d6118a0d026581cfcb27fe9126
sarvani-byteridge <sarvanih@byteridge.com>
2024-02-20 16:43:50 +0530

    Revert "initial commit"
    This reverts commit 40a2a3a00f413d57020d5b68e35c36a4669e9e49.

--- src/app/app.config.ts
+++
@@ -1,9 +0,0 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
-import { provideStore } from '@ngrx/store';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideStore()]
-};
